3865250c58cd26b51af8b7733f5902730b5787c3
Cuuuckiii <fanreaccioneselitecraft@gmail.com>
2024-03-12 14:04:16 -0500

    agregar eslint
    Se agrega eslint al proyecto.

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 const $elemento = $('#botonCambiar')
-$elemento.click( function(){
-    limpiarPantalla()
-    ocultarInfo()
-    mostrarCambios();
+$elemento.click(function () {
+  limpiarPantalla()
+  ocultarInfo()
+  mostrarCambios()
 })
 const informacionPagina = $('.informacionPagina')
-function ocultarInfo(){
-    informacionPagina
+function ocultarInfo () {
+  informacionPagina
     .addClass('oculto')
 }
-function mostrarCambios(){
-    const valorSeleccionado = $('#selectMoneda').val();
-    console.log(valorSeleccionado)
-    const urlApiNueva = 'https://v6.exchangerate-api.com/v6/c5f93ef8d82c885c78c67caf/latest/' + valorSeleccionado
-    console.log(urlApiNueva)
-    fetch(urlApiNueva)
+function mostrarCambios () {
+  const valorSeleccionado = $('#selectMoneda').val()
+  console.log(valorSeleccionado)
+  const urlApiNueva = 'https://v6.exchangerate-api.com/v6/c5f93ef8d82c885c78c67caf/latest/' + valorSeleccionado
+  console.log(urlApiNueva)
+  fetch(urlApiNueva)
     .then(respuesta => respuesta.json())
     .then(respuestaJSON => {
-        const fila = $('.filaMonedas')
-        const valorCantidad= Number($('#numeroCalcular').val())
-        Object.keys(respuestaJSON.conversion_rates).forEach(moneda =>{
-            const cantidadConvertida = (Number(respuestaJSON.conversion_rates[moneda])*valorCantidad).toFixed(2)
-            const divMoneda = $('<div></div>').text(miObjeto.monedaa[moneda] + ' ' + cantidadConvertida);
-            divMoneda.addClass("col-6 col-md-2")
-            divMoneda.addClass("elementoMoneda")
-             fila.append(divMoneda);
-        })
-    }) 
+      const fila = $('.filaMonedas')
+      const valorCantidad = Number($('#numeroCalcular').val())
+      Object.keys(respuestaJSON.conversion_rates).forEach(moneda => {
+        const cantidadConvertida = (Number(respuestaJSON.conversion_rates[moneda]) * valorCantidad).toFixed(2)
+        const divMoneda = $('<div></div>').text(miObjeto.monedaa[moneda] + ' ' + cantidadConvertida)
+        divMoneda.addClass('col-6 col-md-2')
+        divMoneda.addClass('elementoMoneda')
+        fila.append(divMoneda)
+      })
+    })
 };
-function limpiarPantalla(){
-    $('.filaMonedas').html('')
+function limpiarPantalla () {
+  $('.filaMonedas').html('')
 }
